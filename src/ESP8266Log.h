4881214f3a56b2217841aad110f50f26f35f6ca3
Pablo Clemente <paclema@gmail.com>
2023-05-19 10:29:34 +0200

    Include ESP_LOGE, ESP_LOGD and ESP_LOGV definitions for esp8266 logging

--- src/ESP8266Log.h
+++ src/ESP8266Log.h
@@ -2,13 +2,28 @@
 #define ESP8266LOG_H
 
 #ifdef ESP8266
+  #define ESP_LOGE(TAG, ...) do { \
+    Serial.printf("[%6lu][E][%s]: ", (unsigned long) (micros() / 1000ULL), TAG); \
+    Serial.printf(__VA_ARGS__); \
+    Serial.println(); \
+  } while (0)
+  #define ESP_LOGW(TAG, ...) do { \
+    Serial.printf("[%6lu][W][%s]: ", (unsigned long) (micros() / 1000ULL), TAG); \
+    Serial.printf(__VA_ARGS__); \
+    Serial.println(); \
+  } while (0)
   #define ESP_LOGI(TAG, ...) do { \
     Serial.printf("[%6lu][I][%s]: ", (unsigned long) (micros() / 1000ULL), TAG); \
     Serial.printf(__VA_ARGS__); \
     Serial.println(); \
   } while (0)
-#define ESP_LOGW(TAG, ...) do { \
-    Serial.printf("[%6lu][W][%s]: ", (unsigned long) (micros() / 1000ULL), TAG); \
+  #define ESP_LOGD(TAG, ...) do { \
+    Serial.printf("[%6lu][D][%s]: ", (unsigned long) (micros() / 1000ULL), TAG); \
+    Serial.printf(__VA_ARGS__); \
+    Serial.println(); \
+  } while (0)
+  #define ESP_LOGV(TAG, ...) do { \
+    Serial.printf("[%6lu][V][%s]: ", (unsigned long) (micros() / 1000ULL), TAG); \
     Serial.printf(__VA_ARGS__); \
     Serial.println(); \
   } while (0)
